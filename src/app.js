0f42a05259dd3f2817e595e9d28b9f262af6538d
alexi2020 <alexidiaz123alexi@gmail.com>
2022-06-01 12:09:46 -0400

    Conexión Base de datos
    Se realizo la conexion con la base de datos SQL Server

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import './database/connection'
 const express = require('express')
 const config = require('./config')
 const path = require('path')
--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,20 @@
-const sql = require('mssql')
+import sql from 'mssql'
 
 const dbSettings= {
-    user
+    user:'SanFernado',
+    password:'fer123',
+    server:'localhost',
+    database:'Activos',
+    options: {
+        encrypt: true, // for azure
+        trustServerCertificate: true // change to true for local dev / self-signed certs
+      }
 }
 
-sql.connect()
+async function getConnection(){
+const pool=await sql.connect(dbSettings);
+const result= await pool.request().query('SELECT * FROM Ubicacion');
+console.log(result);
+}
+
+getConnection();
