08917e7f91c35ccca375b440e0a6d2465d9ce9da
_javieress <javier.rojas04@alumnos.ucn.cl>
2022-06-07 23:04:16 -0400

    Inicio Frontend
    - Perfil administrador
    - Registrar usuario
    - Registrar activo
    - Registrar lugar
    - Registrar tipo de activo
    ... entre otros

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,22 @@ router.get('/login',(req,res,next) => {
 router.get('/user-register',(req,res,next) => {
     res.render('user-register.ejs',{title: 'Login FIXUM'})
 })
+router.get('/asset-register',(req,res,next) => {
+    res.render('asset-register.ejs',{title: 'Login FIXUM'})
+})
+router.get('/admin-profile',(req,res,next) => {
+    res.render('admin-profile.ejs',{title: 'Login FIXUM'})
+})
+router.get('/user-profile',(req,res,next) => {
+    res.render('user-profile.ejs',{title: 'Login FIXUM'})
+})
+router.get('/place-register',(req,res,next) => {
+    res.render('place-register.ejs',{title: 'Login FIXUM'})
+})
+router.get('/asset-type-register',(req,res,next) => {
+    res.render('asset-type-register.ejs',{title: 'Login FIXUM'})
+})
+
 
 
 // solo prueba del creador de qr
