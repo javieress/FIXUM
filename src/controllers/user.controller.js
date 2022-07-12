fc189bcd91bc880ecbbec32d596e4758ffd07540
alexi2020 <alexidiaz123alexi@gmail.com>
2022-07-12 16:54:25 -0400

    Validacion rut
    Creacion de Usuario Funcionando

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,29 @@ const config = require('../config/dbconfig')
 const sql = require('mssql')
 const user = require('../models/User')
 const bodyParser = require('body-parser')
-//const { default: users } = require('../models/User')
+
+const Fn = {
+	// Valida el rut con su cadena completa "XXXXXXXX-X"
+	validaRut : function (rutCompleto) {
+		if (!/^[0-9]+[-|‐]{1}[0-9kK]{1}$/.test( rutCompleto ))
+			return false;
+		let tmp 	= rutCompleto.split('-');
+		let digv	= tmp[1]; 
+		let rut 	= tmp[0];
+		if ( digv == 'K' ) digv = 'k' ;
+		return (Fn.dv(rut) == digv );
+	},
+	dv : function(T){
+		let M=0,S=1;
+		for(;T;T=Math.floor(T/10))
+			S=(S+T%10*(9-M++%6))%11;
+		return S?S-1:'k';
+	}
+}
+
+
+
+
 
 
 module.exports={
@@ -15,15 +37,21 @@ module.exports={
     post:async function (req,res) {
         let message = "El usuario '"
         
-        
-        const user_esta = await user.findOne(req,res);
-        if(user_esta!=null){
-            message+=req.body['new-user-username']+ "'ya existe.";
-        }
-        else{
-            user.post(req,res)//llamo a funcion post para que cree usuario
-            message+= req.body['new-user-username'] + "' se guardó con éxito."
-        
+
+        if(Fn.validaRut(req.body['new-user-username']) && (req.body['new-user-username'].length==10 || req.body['new-user-username'].length==9)){
+
+            const user_esta = await user.findOne(req,res);
+            if(user_esta!=null){
+                message+=req.body['new-user-username']+ "'ya existe.";
+            }
+            else{
+                user.post(req,res)//llamo a funcion post para que cree usuario
+                message+= req.body['new-user-username'] + "' se guardó con éxito."
+            
+            }
+        }else{
+            message+=req.body['new-user-username']+ "'no es valido";
+            
         }
     
 
