a3cceaadbba6104228781a933e8a1928c0cc0fb3
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2022-07-15 17:03:30 -0400

    controladores añadidos a la vista de select qr

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const printerController = require('../controllers/printer.controller')
 const router = express.Router()
+const asset = require("../controllers/asset.controller");
 
 const qrController = require('../controllers/qr-reader.controller')
 
@@ -23,7 +24,7 @@ router.get('/scan',(req,res,next) => {
     res.render('prueba_LectorQR.ejs',{title: 'Lector QR'})
 })
 router.get('/print',(req,res,next) => {
-    res.render('select_qr_to_print.ejs',{title: ' | Seleccionar Items para Imprimir'})
+    res.render('select_qr_to_print.ejs',{title: ' | Seleccionar Items para Imprimir', assetList: await asset.list()})
 })
 router.post('/print',printerController.create_qr_download)
 router.get('/scan',qrController.index)
