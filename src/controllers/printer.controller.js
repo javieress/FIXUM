985ea30f9070b4f1ecaf9050e99d70d330832480
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2022-07-18 12:07:45 -0400

    prueba de python en heroku
    Comando basico nomas para ver si corre.

--- src/controllers/printer.controller.js
+++ src/controllers/printer.controller.js
@@ -1,14 +1,29 @@
 const { render } = require("ejs");
+const { connected } = require("process");
 
 
 module.exports = {create_qr_download:function(req, res){
     console.log("hola xd");
 
-    var spawn = require("child_process").spawn;
+    const { spawn } = require('child_process');
 
-    var pythonProcess = spawn('python',["generador_hojas_impresion.py"]);
+    const child = spawn('python', ['-h']);
 
-    pythonProcess();
+    child.stdout.on('data', (data) => {
+        console.log(`stdout:+ ${data}`);
+    })
+
+    child.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`)
+    })
+
+    child.on('error', (error) => console.log('error: ${error.message}'))
+
+    child.on('exit', (code, signal) => {
+        if (code) console.log("Proceso termino con el codigo: ${code}")
+        if (signal) console.log("Proceso matado con la señal: ${signal}")
+        console.log("Done :)");
+    })
 
 
 
