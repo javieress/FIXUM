349b41d59cc842a2135782d9713b54a475df25bc
Cristhian <cristhian.rabi@alumnos.ucn.cl>
2022-07-18 15:10:41 -0400

    Update printer.controller.js

--- src/controllers/printer.controller.js
+++ src/controllers/printer.controller.js
@@ -3,32 +3,11 @@ const { connected } = require("process");
 
 
 
+
 module.exports = {create_qr_download:function(req, res){
     console.log("hola xd");
 
-<<<<<<< HEAD
-    const { spawn } = require('child_process');
-
-    const child = spawn('python', ['-h']);
-
-    child.stdout.on('data', (data) => {
-        console.log(`stdout:+ ${data}`);
-    })
-
-    child.stderr.on('data', (data) => {
-        console.log(`stderr: ${data}`)
-    })
-
-    child.on('error', (error) => console.log(`error: ${error.message}`))
-
-    child.on('exit', (code, signal) => {
-        if (code) console.log("Proceso termino con el codigo: ${code}")
-        if (signal) console.log("Proceso matado con la señal: ${signal}")
-        console.log("Done :)");
-    })
-=======
->>>>>>> parent of 34fcf23 (.)
-
+    
 
     res.render("./select_qr_to_print.ejs",{title: ' | Seleccionar Items para Imprimir'})
 }}
