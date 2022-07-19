7d75ee9b9a27a0cc4396d6e64ea3dd6de62fd265
_javieress <javier.rojas04@alumnos.ucn.cl>
2022-07-19 16:06:25 -0400

    otro

--- src/app.js
+++ src/app.js
@@ -25,6 +25,7 @@ const app = express()
 app.set('port', config.port) 
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
+app.set('trust proxy', 1)
 // controllers
 
 var dbConfig = {
