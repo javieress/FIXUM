889ac6494d8fa861faf60a8138f72eb411821b46
_javieress <javier.rojas04@alumnos.ucn.cl>
2022-07-19 21:10:22 -0400

    arreglo pagina de error

--- src/app.js
+++ src/app.js
@@ -64,9 +64,6 @@ app.use('/auth',authsRouter)
 app.use((err,req,res,next) => {
     res.send('Ha ocurrido un error')
 })
-app.use(function(req, res, next) { 
-    res.status(404); 
-    return res.send('404').redirect('/'); });
 
 app.use('/edit',editRouter)
 
