9a03026b553679bd3d0bf2e2c5c0dee411942f32
alexi2020 <alexidiaz123alexi@gmail.com>
2022-07-20 15:45:21 -0400

    Cambio Predeterminado
    cambio cargo predeterminado para que no se muestre en la vista de edicion de cargos

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -11,19 +11,19 @@ router.get('/',auth.verifyToken,auth.isAdmin,async (req,res,next) => {
 })
 router.get('/last-10-added',auth.verifyToken,auth.isAdmin,async (req,res,next) => {
     const top10Assets = await asset.last10Added()
-    res.render('./reports/last-10-added.ejs',{title: ' | Reportes', last10AssetAdded: await top10Assets, navBar: await auth.navigationBar(req)})
+    res.render('./reports/last-10-added.ejs',{title: ' | Ultimas Modificaciones', last10AssetAdded: await top10Assets, navBar: await auth.navigationBar(req)})
 })
 router.get('/totalByAssets',auth.verifyToken,auth.isAdmin,async (req,res,next) => {
     const totalByAssets = await asset.AssetsPrices()
-    res.render('./reports/totalByAssets.ejs',{title: ' | Reportes', totalByAssets: await totalByAssets, navBar: await auth.navigationBar(req)})
+    res.render('./reports/totalByAssets.ejs',{title: ' | Total/Activo', totalByAssets: await totalByAssets, navBar: await auth.navigationBar(req)})
 })
 router.get('/totalByAssetsTypes',auth.verifyToken,auth.isAdmin,async (req,res,next) => {
     const totalByAssetsTypes = await assetTypes.TotalByAssetsTypes()
-    res.render('./reports/totalByAssetsTypes.ejs',{title: ' | Reportes', totalByAssetsTypes: await totalByAssetsTypes, navBar: await auth.navigationBar(req)})
+    res.render('./reports/totalByAssetsTypes.ejs',{title: ' | Total/Tipo Activo', totalByAssetsTypes: await totalByAssetsTypes, navBar: await auth.navigationBar(req)})
 })
 router.get('/totalAssetsByLocation',auth.verifyToken,auth.isAdmin,async (req,res,next) => {
     const totalAssetsByLocation = await asset.TotalAssetsByLocation()
-    res.render('./reports/totalAssetsByLocation.ejs',{title: ' | Reportes', totalAssetsByLocation: await totalAssetsByLocation, navBar: await auth.navigationBar(req)})
+    res.render('./reports/totalAssetsByLocation.ejs',{title: ' | Total/Ubicacion', totalAssetsByLocation: await totalAssetsByLocation, navBar: await auth.navigationBar(req)})
 })
 
 
