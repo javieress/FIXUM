bad996f222c255bd5bf5796e97d329a6c4458d4d
nicopina <64911016+nicopina@users.noreply.github.com>
2022-07-21 21:22:09 -0400

    arreglo contacto

--- src/routes/registers.js
+++ src/routes/registers.js
@@ -71,7 +71,7 @@ router.get('/userPosition-edit/:id',auth.verifyToken,auth.isAdmin, async (req,re
 })
 
 // Ruta de contacto
-router.post('/contact',auth.verifyToken,mailController.contact)
+router.post('/contact',mailController.contact)
 
 // Ruta de notificaciones
 router.post('/notification',auth.verifyToken,auth.isAdmin,mailController.adminNotification)
